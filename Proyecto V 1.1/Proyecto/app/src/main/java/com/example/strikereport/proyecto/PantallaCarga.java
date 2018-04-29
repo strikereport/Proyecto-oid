a7e27d88a1134d7f62956720222deafbd434d76d
strikereport <34009803+strikereport@users.noreply.github.com>
2018-04-29 11:35:52 -0300

    Inicio
    se agrega el inicio a la pantalla de carga

--- Proyecto V 1.1/Proyecto/app/src/main/java/com/example/strikereport/proyecto/PantallaCarga.java
+++ Proyecto V 1.1/Proyecto/app/src/main/java/com/example/strikereport/proyecto/PantallaCarga.java
@@ -1,5 +1,6 @@
 package com.example.strikereport.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,22 @@ public class PantallaCarga extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_carga);
+        //para iniciar la siguiente pantalla
+        final Intent i = new Intent(this,Inicio.class);
+        //creamos un hilo para esa ejecucion
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
